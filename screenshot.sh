b879ca2f5a0c2e4bbd1259d8f712c745cb8a7984
gabriel <gfranklings@gmail.com>
2021-05-09 14:30:03 -0400

    enchancements

--- screenshot.sh
+++ screenshot.sh
@@ -3,19 +3,38 @@
 screenshot_name=`zenity --title 'Screenshot' --entry --text 'Write the name of screenshot'`
 
 if [[ $screenshot_name == '' ]]; then
+    echo "Exiting..."
     exit 0
 fi
 
 screenshot_url=`zenity --title 'Open a folder' --file-selection --directory`
 
-
 if [[ $screenshot_url == '' ]]; then
+    echo "Exiting..."
+    exit 0
+fi
+
+screenshot_manager=$(
+    zenity --title "Screenshot Manager" --forms \
+        --text "Select screenshot manager" \
+        --add-combo="Select screenshot manager" \
+        --combo-values="scrot|import"
+)
+
+if [[ $screenshot_manager == '' ]]; then
+    echo "Exiting..."
     exit 0
 fi
 
 sleep 0.5
 
 # Creating the screenshot
-scrot "$screenshot_name" -e "mv \$f ${screenshot_url}"
+
+if [[ "$screenshot_manager" == "scrot" ]]; then
+    scrot "$screenshot_name" -e "mv \$f ${screenshot_url}"
+elif [[ "$screenshot_manager" == "import" ]]; then
+    import "$screenshot_name"
+    mv "$screenshot_name" "$screenshot_url"
+fi
 
 zenity --title 'Success' --info --text 'Screenshot created successfully!'
